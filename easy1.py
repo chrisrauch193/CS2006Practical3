0cbcd1af6356fa7dc06fd5729b59c7eddcc6fb31
Jalaj Khandelwal <jalajkhandelwal@8afbdaad.st-andrews.ac.uk>
2017-03-28 21:50:17 +0100

    import has been edited to have all of the working code which we will be usin g in the jupyter notebook, copies of all of the code are included in the files a nalysis.py, easy1.py, easy2.py Furthermore, there are snippets of the code at the end of the Jupyter Notebook

--- easy1.py
+++ easy1.py
@@ -1,8 +1,25 @@
-easy12 = df.groupby(['Occupation','Approximated Social Grade']).count()
-easy12
+#Easy Extension 1 part 1 Using crosstab
+import pandas as pd
 
-easy11 = df.groupby(['Region','Industry']).count()
-easy11
+def easy11Crosstab(df):
+    dfeasy11 = pd.crosstab(df['Region'], df['Industry'])
+    return str(dfeasy11)
 
-easy12 = pd.crosstab(df['Occupation'], df['Approximated Social Grade'])
-easy11 = pd.crosstab(df['Region'], df['Industry']) 
+def easy12Crosstab(df):
+    #Easy Extension 1 part 2 using crosstab
+    dfeasy12 = pd.crosstab(df['Occupation'], df['Approximated Social Grade'])
+    return str(dfeasy12)
+
+def easy11GroupBy(df):
+    #Easy 1 part 1 using GroupBy
+    easy11 = df.groupby(['Region','Industry'])['Person ID'].count()
+    easy11 = pd.Series.to_frame(easy11)
+    easy11.columns.values[0] = 'Counts'
+    return str(easy11)
+
+def easy12GroupBy(df):
+    #Easy 1 part 2 using GroupBy
+    easy12 = df.groupby(['Occupation','Approximated Social Grade'])['Person ID'].count()
+    easy12 = pd.Series.to_frame(easy12)
+    easy12.columns.values[0] = 'Counts'
+    return str(easy12)
--- easy2.py
+++ easy2.py
@@ -1,7 +1,15 @@
-ea = df[df["Economic Activity"].isin(range(0, 5))]
+import pandas as pd
 
-easy21 = pd.crosstab(index = (ea)['Region'], columns = 'count')
-easy21
+def easy21(df):
+    #Filtering the Dataset to be economic code from 1-4
+    ea = df[df["Economic Activity"].isin(range(1, 5))]
+    # Easy 2 Part 1 using CrossTab
+    easy21 = pd.crosstab(index = (ea)['Region'], columns = 'count')
+    return str(easy21)
 
-easy22 = pd.crosstab(index = (ea)['Age'], columns = 'count')
-easy22
+def easy22(df):
+    #Filtering the Dataset to be economic code from 1-4
+    ea = df[df["Economic Activity"].isin(range(1, 5))]
+    # Easy 2 Part 2 using CrossTab
+    easy22 = pd.crosstab(index = (ea)['Age'], columns = 'count')
+    return str(easy22)
