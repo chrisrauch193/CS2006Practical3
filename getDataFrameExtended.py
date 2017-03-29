6705359705386cfe813975f11da1d4745e7d2dae
Jalaj Khandelwal <jalajkhandelwal@8afbc4ac.st-andrews.ac.uk>
2017-03-29 17:24:36 +0100

    Able to read all of the files in the directory and concatonate and also add the match and venue columns

---
+++ getDataFrameExtended.py
@@ -0,0 +1,24 @@
+import pandas as pd
+import numpy as np
+import os
+import csv
+
+def getData():
+    path = './TestingFiles/'
+    filelist = os.listdir(path)
+    frame = pd.DataFrame()
+    Venue = 0
+    matchno = 0;
+    for file in filelist:
+        with open(path+file,"r") as f:
+            csvObj = csv.reader(f)
+            csvObj = list(csvObj)
+            matchno = csvObj[7][2]
+            venue = csvObj[8][2]
+        df=pd.read_csv(path + file,skiprows =20,names = ["Type", "Innings", "Ball No.", "Batting Team", "Striker", "Non - Striker", "Bowler", "Runs", "Extras", "How Out", "Batsman Out"])
+        df.insert(0, 'Match Number', matchno)
+        df.insert(1,'Venue',venue)
+        del df['Type']
+        frame = pd.concat([frame,df], ignore_index=True)
+    frame = frame.replace(np.nan, '-', regex=True)
+    return frame
