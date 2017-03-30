4fcbdfbba9bc084a622f65620ee5bc8c0861e74c
Jalaj Khandelwal <jalajkhandelwal@8afbf241.st-andrews.ac.uk>
2017-03-30 09:35:32 +0100

    winner added to the data for further data analysis

--- getDataFrameExtended.py
+++ getDataFrameExtended.py
@@ -7,17 +7,20 @@ def getData():
     path = './TestingFiles/'
     filelist = os.listdir(path)
     frame = pd.DataFrame()
-    Venue = 0
+    venue = 0
     matchno = 0;
+    winner = ''
     for file in filelist:
         with open(path+file,"r") as f:
             csvObj = csv.reader(f)
             csvObj = list(csvObj)
             matchno = csvObj[7][2]
             venue = csvObj[8][2]
+            winner = csvObj[18][2]
         df=pd.read_csv(path + file,skiprows =20,names = ["Type", "Innings", "Ball No.", "Batting Team", "Striker", "Non - Striker", "Bowler", "Runs", "Extras", "How Out", "Batsman Out"])
         df.insert(0, 'Match Number', matchno)
-        df.insert(1,'Venue',venue)
+        df.insert(1,'Winner',winner)
+        df.insert(2,'Venue',venue)
         del df['Type']
         frame = pd.concat([frame,df], ignore_index=True)
     frame = frame.replace(np.nan, '-', regex=True)
